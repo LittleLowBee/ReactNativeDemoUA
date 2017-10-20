a43407f8dcaf50e568a9b3d1aba8d69eb0c7fc29
MagnumSong <983174628@qq.com>
2017-10-20 09:20:54 +0800

    some changes

--- MagnumDemo/App.js
+++ MagnumDemo/App.js
@@ -6,14 +6,35 @@
 
 
 import React, { Component } from 'react';
+import {
+    Text,
+    View,
+} from 'react-native';
+import {
+    StackNavigator
+} from 'react-navigation';
 
+
+class HomeScreen extends React.Component {
+    static navigationOptions = {
+        title: '首页',
+    };
+    render() {
+        return (
+            <View>
+              <Text>来啊，快活呀!</Text>
+            </View>
+        );
+    }
+}
+
+export const SimpleApp = StackNavigator({
+    Home: { screen: HomeScreen }
+});
 
 export default class App extends Component<{}> {
 
   render() {
-    return (<MyApp/>);
+    return (<SimpleApp/>);
   }
 }
-
-
-
